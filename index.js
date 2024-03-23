74caf66c32331b808b8c6cea19d47479302beb78
zainab-ibrahim <163460671+zainab-ibrahim@users.noreply.github.com>
2024-03-23 10:45:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//Data Types
+//example 1 =string
+var personname = "zainab";
+//example 2 =number
+var personnumber = 123;
+//example 3 =boolean
+var condition = true;
+//example 4 =null
+var nothing = null;
+//example 5 =undifine
+var novalue;
+console.log(personname);
+console.log(personnumber);
+console.log(condition);
+console.log(nothing);
+console.log(novalue);
---
+++ index.ts
@@ -0,0 +1,22 @@
+//Data Types
+
+//example 1 =string
+let personname :string="zainab"
+
+//example 2 =number
+let personnumber:number=123
+
+//example 3 =boolean
+let condition:boolean=true
+
+//example 4 =null
+let nothing:null=null
+
+//example 5 =undifine
+let novalue:undefined
+
+console.log(personname);
+console.log(personnumber);
+console.log(condition);
+console.log(nothing);
+console.log(novalue);
